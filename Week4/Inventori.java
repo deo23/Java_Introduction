d0ff2190ebf63dd55e85c178feccf3f32103d31d
deo23 <95121147+deo23@users.noreply.github.com>
2022-02-26 13:13:19 +0700

    Create Inventori.java

---
+++ Week4/Inventori.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * @author Deo
+ */
+public class Inventori {
+    Barang[] barangs;
+    
+    void initBarang() {
+        barangs = new Barang[2];
+        barangs[0] = new Barang("001", "Baju", 10);
+        barangs[1] = new Barang("002", "Celana", 20);
+    }
+    
+    void showBarang() {
+        System.out.println(barangs[0].nama_barang + "(" + barangs[0].getStok() + ")");
+        System.out.println(barangs[1].nama_barang + "(" + barangs[1].getStok() + ")");
+    }
+    
+    void pengadaan() {
+        initBarang();
+        barangs[0].plusStok(20);// add stok with method
+        //barangs[0].stok -= 30; Can't do the operations because it's private
+        //barangs[0].stok *= 30; Can't do the operations because it's private
+        showBarang();  
+    }
+    
+    public static void main(String[] args) {
+        Inventori beli = new Inventori();
+        beli.pengadaan();
+    }
+}
