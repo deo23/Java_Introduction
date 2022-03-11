28b4d66ec752f38b4d8ebaacc46545704dd0960b
deo23 <95121147+deo23@users.noreply.github.com>
2022-03-11 15:54:08 +0700

    Create NarutoAnime.java

---
+++ Week5/NarutoAnime.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Deo
+ */
+public class NarutoAnime {
+    public static void main(String[] args) {
+      Sasuke s = new Sasuke();
+      s.printDojutsu();
+      Itachi i = new Sasuke();
+      i.printKekkeiGenkai();
+    }
+}
