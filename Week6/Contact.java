b46c81335446b13c41382dd86445803135d1c8a1
deo23 <95121147+deo23@users.noreply.github.com>
2022-03-20 16:37:06 +0700

    Create Contact.java

---
+++ Week6/Contact.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author ginaanifahchoirunnisa
+ * @modified by Deo
+ */
+public class Contact {
+    private String phone;
+	
+	public Contact(String phone) {
+		this.phone = phone;
+	}
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+	
+}
