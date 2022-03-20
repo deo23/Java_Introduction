11cfdb02330927442bf0cc4a9eea92ab11a8b5f7
deo23 <95121147+deo23@users.noreply.github.com>
2022-03-20 16:37:28 +0700

    Create GPA.java

---
+++ Week6/GPA.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author ginaanifahchoirunnisa
+ * @modified by Deo
+ */
+public class GPA {
+    private double gpa;
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+	
+    public GPA(double grade) {
+	this.gpa = grade;
+	}
+
+	
+}
