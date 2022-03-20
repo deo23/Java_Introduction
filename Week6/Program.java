ecf6abb93a4d9f3007399ff3178b85edb905c56c
deo23 <95121147+deo23@users.noreply.github.com>
2022-03-20 16:38:10 +0700

    Create Program.java

---
+++ Week6/Program.java
@@ -0,0 +1,23 @@
+import java.util.*;
+/**
+ *
+ * @author ginaanifahchoirunnisa
+ * @modified by Deo
+ */
+public class Program {
+    private String programName;
+	
+	public Program(String program) {
+		this.programName = program;
+	}
+
+    public String getProgramName() {
+        return programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+	
+}
