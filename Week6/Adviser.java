b3ada528094f0b09fa9d4256f834d5e5d110bd69
deo23 <95121147+deo23@users.noreply.github.com>
2022-03-20 16:36:26 +0700

    Create Adviser.java

---
+++ Week6/Adviser.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author ginaanifahchoirunnisa
+ * @modified by Deo
+ */
+public class Adviser {
+    private String adviser;
+	
+	public Adviser(String adviser) {
+		this.adviser = adviser;
+	}
+
+    public String getAdviser() {
+        return adviser;
+    }
+
+    public void setAdviser(String adviser) {
+        this.adviser = adviser;
+    }
+
+	
+}
